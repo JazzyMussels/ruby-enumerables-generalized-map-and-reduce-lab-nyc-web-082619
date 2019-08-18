56946fec1b5d5739ef8491d89de065ad8433e88f
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-18 17:32:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,20 +10,5 @@ new
 end
 
 def reduce(source_array, starting_point=0)
-   if source_array.all?{|x| x.is_a?(Integer)}
-     count = starting_point
-     i = 0 
-   while i < source_array.length 
-   count += (source_array[i])
-   i += 1 
-   end
-   return count 
-  else if 
-    source_array.all?{|x| return true if yield(x) == true}
-  else if source_array.any?{|x| return false if yield(x) == false || yield(x) == nil}
-  else 
-   return true 
-  end
-end 
-end 
+ 
 end
