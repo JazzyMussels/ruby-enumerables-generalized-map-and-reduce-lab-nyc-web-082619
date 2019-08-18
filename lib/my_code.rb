f6080aa277293d75f9283294a30b42e6ce3c480e
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-18 17:01:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+def map(array)
+  new = []
+  i = 0 
+  while i < array.length 
+  new << yield(array[i])
+  i += 1 
+end 
+new 
+end
+
+def reduce(source_array, starting_point=0)
+   if source_array.all?{|x| x.is_a?(Integer)}
+     count = starting_point
+     i = 0 
+   while i < source_array.length 
+   count += (source_array[i])
+   i += 1 
+   end
+   return count 
+  else if 
+    source_array.all?{|x| return true if yield(x) == true}
+  else if source_array.all?{|x| return false if yield(x) == false || yield(x) == nil}
+  else 
+   return true 
+  end
+end 
+end 
+end
